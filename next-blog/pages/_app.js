608747e4bf9bb6eba8626edd1fd30d42bc7694c1
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-10-09 11:43:38 +0200

    public folder

--- next-blog/pages/_app.js
+++ next-blog/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import NavBar from "../components/NavBar";
 import "../styles/globals.css";
@@ -5,6 +6,9 @@ import "../styles/globals.css";
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <header>
         <NavBar />
       </header>
