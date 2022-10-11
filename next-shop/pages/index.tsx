1424a25b7d189f5ae37141d30bfa3562e29c7607
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-10-11 07:46:07 +0200

    New project with TS

---
+++ next-shop/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main>
+        <h1>Next Shop</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
