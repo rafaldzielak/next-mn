d2058ed5873c3b4c278fa800fe3da8bb7ecac6fd
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-10-14 18:47:19 +0200

    React query setup

--- next-shop/pages/_app.tsx
+++ next-shop/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
